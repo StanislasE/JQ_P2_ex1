b7e806549befd3d275d6529debd79a2028ea6a15
StanislasE <stanislas.etienne@gmail.com>
2020-11-06 16:09:03 +0100

    fait

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.img').mouseover(function(){
+            $('img').height(700);
+        });
+    $('.img').mouseout(function(){
+            $('img').height(500);
+        });
+});
