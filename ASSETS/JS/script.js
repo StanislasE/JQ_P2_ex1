accf733bb246d9fa305bed366d0316c2b9e567cf
StanislasE <stanislas.etienne@gmail.com>
2020-11-09 13:55:16 +0100

    fait

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,6 +1,7 @@
 $(function(){
-    $('.img').mouseover(function(){  // Quand le curseur est sur l'image, cela déclanche un agrandissement
-            $('img').height(700);    // ( x ) valeur x en px
+    $('.img').mouseover(function(){  // Quand le curseur est sur l'image (class img), cela déclanche un agrandissement
+            // $('img').height(700);    // ( x ) valeur x en px
+            $(this).height(700);        // 'this' cible chaque img indépendamment
         });
     $('.img').mouseout(function(){   // Quand le curseur est sur l'image, cela déclanche un rétrécissement
             $('img').height(500);
